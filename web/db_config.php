cb2e1247a5d49f0e2f167aca68e63145d1f3190b
Autumn <78074803+autumnlindberg1@users.noreply.github.com>
2024-02-04 17:38:12 -0800

    db_config

--- web/db_config.php
+++ web/db_config.php
@@ -1,15 +1,19 @@
 <?php
 
-$env = parse_ini_file('../.env');
 
 /* config vars */
-//$url = parse_url(getenv("DATABASE_URL"));
-//$url = parse_url($env["DATABASE_URL"]);
 
+$serverName = getenv("HOST");
+$userName = getenv("USER");
+$pw = getenv("PASS");
+$db = getenv("DB");
+
+/*
+$env = parse_ini_file('../.env');
 $serverName = $env["HOST"];
 $userName = $env["USER"];
 $pw = $env["PASS"];
 $db = $env["DB"];
-
+*/
 
 ?>
